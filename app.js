40d1e4f8d482db496af2c29ba71cea5e7d1f775c
Dennie Nguyen <streamvillecanada@gmail.com>
2021-08-12 14:00:20 -0400

    add middleware and login layout

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+// >>>>>> Handling traffic to the server <<<<<<
 const express = require('express');
 const app = express();
 const port = 3000;
 const middleware = require('./middleware')
+const path = require('path');
 
 const server = app.listen(port, () => console.log('Server listening on port ' + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+// >>>>> Serving public files <<<<<
+app.use(express.static(path.join(__dirname, "public")));
+
+// >>>>> Routes <<<<<
+const loginRoute = require('./routes/loginRoutes');
+app.use('/login', loginRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
