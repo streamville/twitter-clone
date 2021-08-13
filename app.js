0f7bb3f4c0df4aaf655ae69aa259c45841650362
Dennie Nguyen <streamvillecanada@gmail.com>
2021-08-13 15:59:20 -0400

    install and setup mongoDB

--- app.js
+++ app.js
@@ -4,18 +4,29 @@ const app = express();
 const port = 3000;
 const middleware = require('./middleware')
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const connectDB = require("./config/db");
 
 const server = app.listen(port, () => console.log('Server listening on port ' + port));
+ 
+// Connect to MongoDB
+connectDB();
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+// >>>>> setting up body-parser <<<<<
+app.use(bodyParser.urlencoded({ extended: false }));
 // >>>>> Serving public files <<<<<
 app.use(express.static(path.join(__dirname, "public")));
 
 // >>>>> Routes <<<<<
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
+
 app.use('/login', loginRoute);
+app.use('/register', registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
